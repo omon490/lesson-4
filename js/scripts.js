d2bfdcfe4c65aa9ec44fa91d630b0b4ecf7a6e54
omon490 <omonbekmurodov@gmail.com>
2020-12-11 18:45:43 +0500

    add fontawsome

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+var tempForm = document.querySelector(`.js-temp-form`);
+if (tempForm) {
+  var tempInputCelsiy = tempForm.querySelector(`.js-temp-input-celsiy`);
+  var tempInputFrang = tempForm.querySelector(`.js-temp-input-frang`);
+};
+
+if (tempForm) {
+  tempForm.addEventListener(`submit`, function(evt) {
+    evt.preventDefault();
+    var celsiy = Number(tempInputCelsiy.value);
+    var natija = celsiy * 9 / 5 + 32;
+
+    tempInputFrang.value = natija;
+  });
+};
+
+
+
+// gregrogerg
+
+var elTravelForm = document.querySelector(`.travel-form`);
+var scorePiyoda = 3.6;
+var scoreBycicle = 20.1;
+var scoreCar = 70;
+var scorePlane = 800;
+
+if (elTravelForm) {
+  var elTravelInput = elTravelForm.querySelector(`.demo-input`);
+  var elPiyoda = document.querySelector(`.span-1`);
+  var elBicycle = document.querySelector(`.span-2`);
+  var elCar = document.querySelector(`.span-3`);
+  var elPlane = document.querySelector(`.span-4`);
+
+};
+
+if (elTravelForm) {
+  elTravelForm.addEventListener(`submit`, function(evt){
+
+    evt.preventDefault();
+    var travelLength = Number(elTravelInput.value);
+    elPiyoda.textContent =  `${Math.floor(travelLength / scorePiyoda)} soat ${Math.floor(((travelLength / scorePiyoda) * 60) % 60)} minut ${Math.floor((((travelLength / scorePiyoda) * 60) * 60) % 60)}sek`;
+
+    elBicycle.textContent =  `${Math.floor(travelLength / scoreBycicle)} soat ${Math.floor(((travelLength / scoreBycicle) * 60) % 60)} minut ${Math.floor((((travelLength / scoreBycicle) * 60) * 60) % 60)}sek`;
+
+    elCar.textContent =  `${Math.floor(travelLength / scoreCar)} soat ${Math.floor(((travelLength / scoreCar) * 60) % 60)} minut ${Math.floor((((travelLength / scoreCar) * 60) * 60) % 60)}sek`;
+
+    elPlane.textContent =  `${Math.floor(travelLength / scorePlane)} soat ${Math.floor(((travelLength / scorePlane) * 60) % 60)} minut ${Math.floor((((travelLength / scorePlane) * 60) * 60) % 60)}sek`;
+  });
+};
